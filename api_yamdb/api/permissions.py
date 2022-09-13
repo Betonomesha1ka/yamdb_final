0e2cb8e453ca43a5d85274fa9f97726c1e5fa40a
Betonomesha1ka <iudo4ka@mail.ru>
2022-09-13 21:38:43 +0500

    Крайне приличный текст

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -19,6 +19,7 @@ class IsAuthorOrAdminOrModeratorOnly(permissions.BasePermission):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated)
 
+
 class IsAdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
